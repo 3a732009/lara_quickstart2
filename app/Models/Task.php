4fb88bb7c5f8149db92d4ef2701c5ac9f94a36a6
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-15 08:46:24 +0800

    建立Task model,建立User及Task兩個 models 之間具有一對多的關係

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * 這些屬性能被批量賦值。
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
