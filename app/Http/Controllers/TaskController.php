27c2b56372c945e74b39329fbb18f60addb9f00e
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-12-17 17:44:59 +0800

    新增任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -21,7 +21,20 @@ class TaskController extends Controller
     }
     public function index(Request $request)
     {
-        return view('tasks.index');
-    }
+        $tasks = Task::where('user_id', $request->user()->id)->get();
 
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create($request->all());
+
+        return redirect('/tasks');
+    }
 }
